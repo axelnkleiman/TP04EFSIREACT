7e48c9e1c92756e721e273a7807c4e382491d980
AXEL NICOLÁS KLEIMAN <47434251@ort>
2024-05-20 14:36:32 -0300

    1.0

---
+++ src/views/examples/ProfilePage.js
@@ -0,0 +1,402 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.3.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Label,
+  FormGroup,
+  Input,
+  NavItem,
+  NavLink,
+  Form,
+  Nav,
+  TabContent,
+  TabPane,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import ProfilePageHeader from "components/Headers/ProfilePageHeader.js";
+import DemoFooter from "components/Footers/DemoFooter.js";
+
+function ProfilePage() {
+  const [activeTab, setActiveTab] = React.useState("1");
+
+  const toggle = (tab) => {
+    if (activeTab !== tab) {
+      setActiveTab(tab);
+    }
+  };
+
+  document.documentElement.classList.remove("nav-open");
+  React.useEffect(() => {
+    document.body.classList.add("landing-page");
+    return function cleanup() {
+      document.body.classList.remove("landing-page");
+    };
+  });
+  return (
+    <>
+      <ExamplesNavbar />
+      <ProfilePageHeader />
+      <div className="section profile-content">
+        <Container>
+          <div className="owner">
+            <div className="avatar">
+              <img
+                alt="..."
+                className="img-circle img-no-padding img-responsive"
+                src={require("assets/img/faces/axel.kleiman.jpg")}
+              />
+            </div>
+            <div className="name">
+              <h4 className="title">
+                Axel Kleiman <br />
+              </h4>
+              <h6 className="description">Estudiante de ORT</h6>
+            </div>
+          </div>
+          <Row>
+            <Col className="ml-auto mr-auto text-center" md="6">
+              <p>
+                Hola!! Soy Axel Kleiman, tengo 17 años. Me gusta mucho el futbol,
+                soy hincha del Club Atletico Boca Juniors, como toda persona de bien.
+                Voy a la especializacion de informatica
+              </p>
+              <br />
+            </Col>
+          </Row>
+          <br />
+          <div className="nav-tabs-navigation">
+            <div className="nav-tabs-wrapper">
+              <Nav role="tablist" tabs>
+                <NavItem>
+                  <NavLink
+                    className={activeTab === "1" ? "active" : ""}
+                    onClick={() => {
+                      toggle("1");
+                    }}
+                  >
+                    Follows
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className={activeTab === "2" ? "active" : ""}
+                    onClick={() => {
+                      toggle("2");
+                    }}
+                  >
+                    Following
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </div>
+          </div>
+          {/* Tab panes */}
+          <TabContent className="following" activeTab={activeTab}>
+            <TabPane tabId="1" id="follows">
+              <Row>
+                <Col className="ml-auto mr-auto" md="6">
+                  <ul className="list-unstyled follows">
+                    <li>
+                      <Row>
+                        <Col className="ml-auto mr-auto" lg="2" md="4" xs="4">
+                          <img
+                            alt="..."
+                            className="img-circle img-no-padding img-responsive"
+                            src={require("assets/img/faces/clem-onojeghuo-2.jpg")}
+                          />
+                        </Col>
+                        <Col className="ml-auto mr-auto" lg="7" md="4" xs="4">
+                          <h6>
+                            Ivan Lataschef <br />
+                            <small>Jugador de Fortnite</small>
+                          </h6>
+                        </Col>
+                        <Col className="ml-auto mr-auto" lg="3" md="4" xs="4">
+                          <FormGroup check>
+                            <Label check>
+                              <Input
+                                defaultChecked
+                                defaultValue=""
+                                type="checkbox"
+                              />
+                              <span className="form-check-sign" />
+                            </Label>
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                    </li>
+                    <hr />
+                    <li>
+                      <Row>
+                        <Col className="mx-auto" lg="2" md="4" xs="4">
+                          <img
+                            alt="..."
+                            className="img-circle img-no-padding img-responsive"
+                            src={require("assets/img/faces/ayo-ogunseinde-2.jpg")}
+                          />
+                        </Col>
+                        <Col lg="7" md="4" xs="4">
+                          <h6>
+                            Uriel Cwirenbaum <br />
+                            <small>Uri</small>
+                          </h6>
+                        </Col>
+                        <Col lg="3" md="4" xs="4">
+                          <FormGroup check>
+                            <Label check>
+                              <Input defaultValue="" type="checkbox" />
+                              <span className="form-check-sign" />
+                            </Label>
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                    </li>
+                  </ul>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane className="text-center" tabId="2" id="following">
+              <h3 className="text-muted">Not following anyone yet :(</h3>
+              <Button className="btn-round" color="warning">
+                Find artists
+              </Button>
+            </TabPane>
+          </TabContent>
+        </Container>
+      </div>
+      <div className="section section-dark text-center">
+          <Container>
+            <h2 className="title">Cosas Sobre Mi</h2>
+            <Row>
+              <Col md="4">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={require("assets/img/faces/ort.png")}
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">ORT Yatay</CardTitle>
+                        <h6 className="card-category">Escuela Secundaria -Por Ahora-</h6>
+                      </div>
+                    </a>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-twitter" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-google-plus" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+              <Col md="4">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={require("assets/img/faces/macabi.png")}
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">OHA Macabi</CardTitle>
+                        <h6 className="card-category">Club</h6>
+                      </div>
+                    </a>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-twitter" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-google-plus" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+              <Col md="4">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={require("assets/img/faces/boca.png")}
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Boca Juniors</CardTitle>
+                        <h6 className="card-category">AGUANTE BOCA</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      DALE DALE DALE BOOO
+                      HOY TE VINIMO A ALENTAR 
+                      PARA SER CAMPEON 
+                      HOY HAY QUE GANAR
+                    </p>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-twitter" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-google-plus" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="section landing-section">
+          <Container>
+            <Row>
+              <Col className="ml-auto mr-auto" md="8">
+                <h2 className="text-center">Keep in touch?</h2>
+                <Form className="contact-form">
+                  <Row>
+                    <Col md="6">
+                      <label>Name</label>
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="nc-icon nc-single-02" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input placeholder="Name" type="text" />
+                      </InputGroup>
+                    </Col>
+                    <Col md="6">
+                      <label>Email</label>
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="nc-icon nc-email-85" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input placeholder="Email" type="text" />
+                      </InputGroup>
+                    </Col>
+                  </Row>
+                  <label>Message</label>
+                  <Input
+                    placeholder="Tell us your thoughts and feelings..."
+                    type="textarea"
+                    rows="4"
+                  />
+                  <Row>
+                    <Col className="ml-auto mr-auto" md="4">
+                      <Button className="btn-fill" color="danger" size="lg">
+                        Send Message
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      <DemoFooter />
+    </>
+  );
+}
+
+export default ProfilePage;
